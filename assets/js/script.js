772d4b94e603e3b5eec39998601c9a41ec4e8553
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-05-31 13:21:00 -0400

    broke the old function into multiple functions to handle different parts of the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+var TaskFormHandler = function(event) {
+  event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // package up data as an object
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
   }
+  formEl.reset();
+  var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+  
+  // send it as an argument to createTaskEl
+  createTaskE1(taskDataObj);
+}
 
-  buttonEl.addEventListener("click", createTaskHandler);
+var createTaskE1 = function (taskDataObj) {
+  // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", TaskFormHandler);
